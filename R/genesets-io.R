ca8fb021cf4e084ca1b1782b5fdd487c4cdffc77
Yunuuuu <yunyunpp96@outlook.com>
2025-05-07 00:33:35 +0800

    cleanup codes

--- R/genesets-io.R
+++ R/genesets-io.R
@@ -5,24 +5,24 @@ write_gmt <- function(gs, path) {
 
 read_gmt <- function(path, ...) {
     data <- strsplit(read_lines(path), "\t", fixed = TRUE)
-    data <- data[lengths(data) > 2L]
-    if (length(data) == 0L) cli::cli_abort("No genesets in {.path {path}}")
-    ids <- vapply(data, .subset, 1L, character(1L), USE.NAMES = FALSE)
-    descriptions <- vapply(data, .subset, 2L, character(1L), USE.NAMES = FALSE)
-    genesets <- lapply(data, function(geneset) {
-        trimws(.subset(geneset, -(1:2)), "both")
-    })
-    new_genesets(genesets, ids = ids, descriptions = descriptions)
+    read_genesets(path, unclass(data))
 }
 
 # https://software.broadinstitute.org/cancer/software/gsea/wiki/index.php/Data_formats#GMX:_Gene_MatriX_file_format_.28.2A.gmx.29
 read_gmx <- function(path, ...) {
     data <- utils::read.table(path, header = FALSE, sep = "\t")
-    data <- data[lengths(data) > 2L]
-    if (length(data) == 0L) cli::cli_abort("No genesets in {.path {path}}")
-    ids <- vapply(data, .subset, 1L, character(1L), USE.NAMES = FALSE)
-    descriptions <- vapply(data, .subset, 2L, character(1L), USE.NAMES = FALSE)
-    genesets <- lapply(data, function(geneset) {
+    read_genesets(path, unclass(data))
+}
+
+read_genesets <- function(path, genesets) {
+    genesets <- genesets[lengths(genesets) > 2L]
+    if (length(genesets) == 0L) cli::cli_abort("No genesets in {.path {path}}")
+    ids <- vapply(genesets, .subset, 1L, character(1L), USE.NAMES = FALSE)
+    descriptions <- vapply(
+        genesets, .subset, 2L, character(1L),
+        USE.NAMES = FALSE
+    )
+    genesets <- lapply(genesets, function(geneset) {
         trimws(.subset(geneset, -(1:2)), "both")
     })
     new_genesets(genesets, ids = ids, descriptions = descriptions)
