56e9c4776658b61ba139449804feea9f5da32568
Yunuuuu <yunyunpp96@outlook.com>
2025-05-14 19:36:11 +0800

    Use S3 method

--- R/repr-metrics.R
+++ R/repr-metrics.R
@@ -1,12 +1,21 @@
 #' Create ranking metrics representation
 #'
-#' @return A `xbio_metrics` object, which is a named numeric vector.
-methods::setGeneric("repr_metrics", function(metrics, ..., `_arg` = NULL) {
+#' @param metrics A named numeric ranked metrics.
+#' @inheritParams repr_genesets
+#' @return A named numeric vector with class `xbio_metrics`.
+#' @export
+repr_metrics <- function(metrics, ..., `_arg` = NULL) {
     # `_arg` is not used by user, but used by the internal to provide better
     # message
-    `_arg` <- `_arg` %||% "metrics"
-    standardGeneric("repr_metrics")
-})
+    .repr_metrics(metrics, ..., `_arg` = `_arg` %||% "metrics")
+}
+
+.repr_metrics <- function(metrics, ..., `_arg` = NULL) {
+    if (missing(metrics)) {
+        cli::cli_abort("{.arg {`_arg`}} must be provided")
+    }
+    UseMethod("repr_metrics")
+}
 
 new_metrics <- function(x) structure(x, class = "xbio_metrics")
 
@@ -21,83 +30,64 @@ is_valid_metrics <- function(metrics, names) {
     valid
 }
 
-#' @export
-methods::setMethod(
-    "repr_metrics", "ANY",
-    function(metrics, ..., `_arg` = NULL) {
-        cli::cli_abort(paste(
-            "Cannot extract ranking metrics from",
-            "{.obj_type_friendly {metrics}}"
-        ))
-    }
-)
+#' @exportS3Method repr_metrics default
+repr_metrics.default <- function(metrics, ..., `_arg` = NULL) {
+    cli::cli_abort(
+        "Cannot extract ranking metrics from {.obj_type_friendly {metrics}}"
+    )
+}
 
-#' @export
-methods::setMethod(
-    "repr_metrics", "missing",
-    function(metrics, ..., `_arg` = NULL) {
-        cli::cli_abort("{.arg {`_arg`}} must be provided")
-    }
-)
-
-#' @export
-methods::setMethod(
-    "repr_metrics", "xbio_metrics",
-    function(metrics, ..., `_arg` = NULL) metrics
-)
+#' @exportS3Method repr_metrics xbio_metrics
+repr_metrics.xbio_metrics <- function(metrics, ..., `_arg` = NULL) metrics
 
 #########################################################
-#' @export
+#' @exportS3Method repr_metrics numeric
+#' @method repr_metrics numeric
 #' @rdname repr_metrics
-methods::setMethod(
-    "repr_metrics", "numeric",
-    function(metrics, ..., `_arg` = NULL) {
-        if (is.null(nms <- names(metrics))) {
-            cli::cli_abort("{.arg {`_arg`}} must be a named numeric")
-        }
-        new_metrics(metrics[is_valid_metrics(metrics, nms)])
+repr_metrics.numeric <- function(metrics, ..., `_arg` = NULL) {
+    if (is.null(nms <- names(metrics))) {
+        cli::cli_abort("{.arg {`_arg`}} must be a named numeric")
     }
-)
+    new_metrics(metrics[is_valid_metrics(metrics, nms)])
+}
 
-#' @export
+#' @method repr_metrics data.frame
+#' @exportS3Method repr_metrics data.frame
 #' @rdname repr_metrics
-methods::setMethod(
-    "repr_metrics", "data.frame",
-    function(metrics, ..., `_arg` = NULL) {
-        if (ncol(metrics) < 2L) {
-            cli::cli_abort(
-                "{.arg {arg}} must be a data frame of at least 2 columns"
-            )
-        } else if (ncol(metrics) > 2L) {
-            cli::cli_warn("Only the head 2 columns will be used")
-        }
-        nms <- vec_cast(metrics[[1L]], character(),
-            x_arg = sprintf("the 1st column of %s", `_arg`)
+repr_metrics.data.frame <- function(metrics, ..., `_arg` = NULL) {
+    if (ncol(metrics) < 2L) {
+        cli::cli_abort(
+            "{.arg {arg}} must be a data frame of at least 2 columns"
         )
-        metrics <- vec_cast(metrics[[2L]], numeric(),
-            x_arg = sprintf("the 2nd column of %s", `_arg`)
-        )
-        names(metrics) <- nms
-        new_metrics(metrics[is_valid_metrics(metrics, nms)])
+    } else if (ncol(metrics) > 2L) {
+        cli::cli_warn("Only the head 2 columns will be used")
     }
-)
+    nms <- vec_cast(metrics[[1L]], character(),
+        x_arg = sprintf("the 1st column of %s", `_arg`)
+    )
+    metrics <- vec_cast(metrics[[2L]], numeric(),
+        x_arg = sprintf("the 2nd column of %s", `_arg`)
+    )
+    names(metrics) <- nms
+    new_metrics(metrics[is_valid_metrics(metrics, nms)])
+}
 
-#' @export
+
+#' @method repr_metrics character
+#' @exportS3Method repr_metrics character
 #' @rdname repr_metrics
-methods::setMethod(
-    "repr_metrics", "character",
-    function(metrics, ..., `_arg` = NULL) {
-        if (!rlang::is_string(metrics) || !endsWith(tolower(metrics), ".rnk")) {
-            cli::cli_abort(
-                "{.arg {`_arg`}} must be a single string of {.field .rnk} file path"
-            )
-        }
-        data <- read_rnk(metrics)
-
-        # Call the data frame method
-        repr_metrics(metrics = data, ..., `_arg` = `_arg`)
+repr_metrics.character <- function(metrics, ..., `_arg` = NULL) {
+    if (!rlang::is_string(metrics) || !endsWith(tolower(metrics), ".rnk")) {
+        cli::cli_abort(
+            "{.arg {`_arg`}} must be a single string of {.field .rnk} file path"
+        )
     }
-)
+    data <- read_rnk(metrics)
+
+    # Call the data frame method
+    repr_metrics(metrics = data, ..., `_arg` = `_arg`)
+}
+
 
 write_rnk <- function(prerank, path) {
     write_table(
