9fd11137413af4de55e0aed31821c0bacc7daebe
Yunuuuu <yunyunpp96@outlook.com>
2025-05-14 19:44:59 +0800

    register S3 methods

--- R/repr-matrix.R
+++ R/repr-matrix.R
@@ -1,6 +1,9 @@
 #' Extract matrix
 #'
+#' @param matrix Input object used to extract the matrix.
+#' @inheritParams repr_genesets
 #' @return A `xbio_matrix` object, which is a named numeric vector.
+#' @export
 repr_matrix <- function(matrix, ..., `_arg` = NULL) {
     # Tricks to do common work in S3 generic
     .repr_matrix(matrix, ..., `_arg` = `_arg` %||% "matrix")
@@ -17,13 +20,15 @@ new_matrix <- function(x) structure(x, class = "xbio_matrix")
 
 methods::setOldClass("xbio_matrix")
 
-#' @export
+#' @exportS3Method repr_matrix default
 repr_matrix.default <- function(matrix, ..., `_arg` = NULL) {
     cli::cli_abort("Cannot extract matrix from {.obj_type_friendly {matrix}}")
 }
 
-#' @export
+#' @exportS3Method repr_matrix repr_matrix
 repr_matrix.repr_matrix <- function(matrix, ..., `_arg` = NULL) matrix
 
-#' @export
+#' @exportS3Method repr_matrix matrix
+#' @method repr_matrix matrix
+#' @rdname repr_matrix
 repr_matrix.matrix <- function(matrix, ..., `_arg` = NULL) new_matrix(matrix)
