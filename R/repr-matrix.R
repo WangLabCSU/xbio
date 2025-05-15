57242ffa3d7e15c6cdd6e64dc77b1830b8cc984e
Yunuuuu <yunyunpp96@outlook.com>
2025-05-15 21:24:46 +0800

    more methods for matrix-like objects

--- R/repr-matrix.R
+++ R/repr-matrix.R
@@ -2,7 +2,7 @@
 #'
 #' @param matrix Input object used to extract the matrix.
 #' @inheritParams repr_genesets
-#' @return A `xbio_matrix` object, which is a named numeric vector.
+#' @return A matrix object.
 #' @export
 repr_matrix <- function(matrix, ..., `_arg` = NULL) {
     repr_matrix <- function(matrix, ..., `_arg` = NULL) {
@@ -15,18 +15,64 @@ repr_matrix <- function(matrix, ..., `_arg` = NULL) {
     repr_matrix(matrix, ..., `_arg` = `_arg` %||% "matrix")
 }
 
-new_matrix <- function(x) structure(x, class = "xbio_matrix")
-
-methods::setOldClass("xbio_matrix")
-
 #' @export
 repr_matrix.default <- function(matrix, ..., `_arg` = NULL) {
     cli::cli_abort("Cannot extract matrix from {.obj_type_friendly {matrix}}")
 }
 
 #' @export
-repr_matrix.repr_matrix <- function(matrix, ..., `_arg` = NULL) matrix
+#' @rdname repr_matrix
+repr_matrix.matrix <- function(matrix, ..., `_arg` = NULL) matrix
 
 #' @export
 #' @rdname repr_matrix
-repr_matrix.matrix <- function(matrix, ..., `_arg` = NULL) new_matrix(matrix)
+repr_matrix.dgCMatrix <- function(matrix, ..., `_arg` = NULL) matrix
+
+#' @export
+#' @rdname repr_matrix
+repr_matrix.Seurat <- function(matrix, assay = NULL, ...,
+                               dimred = NULL, n_dimred = NULL,
+                               layer = "data") {
+    if (!is.null(dimred)) {
+        # value is expected to be a matrix or matrix-like object with number of
+        # rows equal to ncol(x).
+        mat <- getExportedValue("SeuratObject", "Embeddings")(
+            matrix, reduction = dimred
+        )
+        if (!is.null(n_dimred)) {
+            if (length(n_dimred) == 1L) n_dimred <- seq_len(n_dimred)
+            mat <- mat[, n_dimred, drop = FALSE]
+        }
+        t(mat)
+    } else {
+        getExportedValue("SeuratObject", "GetAssayData")(
+            matrix, assay = assay, layer = layer
+        )
+    }
+}
+
+#' @export
+#' @rdname repr_matrix
+repr_matrix.SummarizedExperiment <- function(matrix, assay = NULL, ...) {
+    getExportedValue("SummarizedExperiment", "assay")(matrix, assay %||% 1L)
+}
+
+#' @export
+#' @rdname repr_matrix
+repr_matrix.SingleCellExperiment <- function(matrix, assay = NULL, ...,
+                                             dimred = NULL, n_dimred = NULL) {
+    if (!is.null(dimred)) {
+        # value is expected to be a matrix or matrix-like object with number of
+        # rows equal to ncol(x).
+        mat <- getExportedValue("SingleCellExperiment", "reducedDim")(
+            matrix, dimred
+        )
+        if (!is.null(n_dimred)) {
+            if (length(n_dimred) == 1L) n_dimred <- seq_len(n_dimred)
+            mat <- mat[, n_dimred, drop = FALSE]
+        }
+        t(mat)
+    } else {
+        getExportedValue("SummarizedExperiment", "assay")(matrix, assay %||% 1L)
+    }
+}
