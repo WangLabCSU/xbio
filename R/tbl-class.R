a2df610d0ec8857ff1e093073d2924d24d65aea0
Yunuuuu <yunyunpp96@outlook.com>
2025-05-16 22:20:45 +0800

    fix `new_gsea_result`

--- R/tbl-class.R
+++ R/tbl-class.R
@@ -2,7 +2,7 @@ new_tbl <- function(x, ..., class = NULL) {
     new_data_frame(x, ..., class = c(class, "tbl"))
 }
 
-new_gsea_result <- function(x, ...) new_tbl(x, class = "xbio_gsea_result")
+new_gsea_result <- function(x, ...) new_tbl(x, ..., class = "xbio_gsea_result")
 
 tbl_sum.xbio_gsea_result <- function(x, ...) {
     c("GSEA Result" = pillar::dim_desc(x))
